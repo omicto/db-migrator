e96e08418bd59a94633db5f9dd80e034f8cde296
Cris <craisochoa22@gmail.com>
2019-09-22 18:35:25 -0600

    Fixed problems with employeesdao

--- src/main/java/com/omicto/database/oracle/dao/EmployeeOracleDao.java
+++ src/main/java/com/omicto/database/oracle/dao/EmployeeOracleDao.java
@@ -17,18 +17,18 @@ public class EmployeeOracleDao implements EmployeeDao {
 
     @Override
     public void save(Employee employee) throws SQLException {
-        PreparedStatement pstmt = con.prepareStatement("INSERT INTO employees VALUES(?,?,?,?,?,?,?,?,?,?,?)");
+        PreparedStatement pstmt = con.prepareStatement("INSERT INTO hr.employees VALUES(?,?,?,?,?,?,?,?,?,?,?)");
         pstmt.setLong(1, employee.employeeId);
         pstmt.setString(2, employee.firstName);
         pstmt.setString(3, employee.lastName);
         pstmt.setString(4, employee.email);
         pstmt.setString(5, employee.phoneNumber);
-        pstmt.setString(6, employee.jobId);
-        pstmt.setLong(7, employee.managerId);
-        pstmt.setDate(8, Date.valueOf(employee.hireDate));
-        pstmt.setDouble(9, employee.salary);
-        pstmt.setDouble(10, employee.commission);
-        pstmt.setLong(11, employee.managerId);
+        pstmt.setDate(6, Date.valueOf(employee.hireDate));
+        pstmt.setString(7, employee.jobId);
+        pstmt.setDouble(8, employee.salary);
+        pstmt.setDouble(9, employee.commission);
+        pstmt.setLong(10, employee.managerId);
+        pstmt.setLong(11, employee.departmentId);
         pstmt.execute();
         pstmt.close();
     }
@@ -60,13 +60,23 @@ public class EmployeeOracleDao implements EmployeeDao {
         pstmt.close();
     }
     private Employee getEmployeeFromResultset(ResultSet rs) throws SQLException {
-        return new Employee(rs.getLong(1),rs.getString(2),rs.getString(3),
-                rs.getString(4),rs.getString(5),rs.getString(6),
-                rs.getLong(7),rs.getDate(8).toLocalDate(),rs.getDouble(9),rs.getDouble(10),rs.getLong(11));
+        return new Employee(
+                rs.getLong(1),
+                rs.getString(2),
+                rs.getString(3),
+                rs.getString(4),
+                rs.getString(5),
+                rs.getDate(6).toLocalDate(),
+                rs.getString(7),
+                rs.getDouble(8),
+                rs.getDouble(9),
+                rs.getLong(10),
+                rs.getLong(11));
+
     }
     @Override
     public List<Employee> getAll() throws SQLException {
-        PreparedStatement pstmt = con.prepareStatement("SELECT * FROM employees");
+        PreparedStatement pstmt = con.prepareStatement("SELECT * FROM hr.employees");
         ResultSet rs = pstmt.executeQuery();
         List<Employee> emps = new LinkedList<>();
         while(rs.next()){
@@ -79,7 +89,7 @@ public class EmployeeOracleDao implements EmployeeDao {
 
     @Override
     public Employee getOne(Long id) throws SQLException {
-        PreparedStatement pstmt = con.prepareStatement("SELECT * FROM employees WHERE employee_id = ?");
+        PreparedStatement pstmt = con.prepareStatement("SELECT * FROM hr.employees WHERE employee_id = ?");
         pstmt.setLong(1, id);
         ResultSet rs = pstmt.executeQuery();
         rs.next();
--- src/main/java/com/omicto/domain/Employee.java
+++ src/main/java/com/omicto/domain/Employee.java
@@ -3,31 +3,29 @@ package com.omicto.domain;
 import java.time.LocalDate;
 
 public class Employee {
-    public Long employeeId;
+    public long employeeId;
     public String firstName;
     public String lastName;
     public String email;
     public String phoneNumber;
-    public String jobId;
-    public Long managerId;
     public LocalDate hireDate;
+    public String jobId;
     public double salary;
     public double commission;
-    public Long departmentId;
+    public long managerId;
+    public long departmentId;
 
-    public Employee(Long employeeId, String firstName, String lastName, String email,
-                    String phoneNumber, String jobId, Long managerId, LocalDate hireDate,
-                    double salary, double commission, Long departmentId) {
+    public Employee(long employeeId, String firstName, String lastName, String email, String phoneNumber, LocalDate hireDate, String jobId, double salary, double commission, long managerId, long departmentId) {
         this.employeeId = employeeId;
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
         this.phoneNumber = phoneNumber;
-        this.jobId = jobId;
-        this.managerId = managerId;
         this.hireDate = hireDate;
+        this.jobId = jobId;
         this.salary = salary;
         this.commission = commission;
+        this.managerId = managerId;
         this.departmentId = departmentId;
     }
 }
