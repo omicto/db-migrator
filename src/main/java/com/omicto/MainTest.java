e78cf5a780cfe8607060a2bccb81d2cfe086ffd5
Sebastian <tevas.dlr@gmail.com>
2019-09-22 19:32:00 -0600

    Refactor and cleanup

--- src/main/java/com/omicto/MainTest.java
+++ src/main/java/com/omicto/MainTest.java
@@ -1,6 +1,7 @@
 package com.omicto;
 
 import com.omicto.database.ConnectionFactory;
+import com.omicto.database.dao.Dao;
 import com.omicto.database.mssql.SimpleQueryExecutor;
 import com.omicto.database.oracle.dao.*;
 import com.omicto.database.oracle.metadata.DdlParser;
@@ -35,51 +36,48 @@ public class MainTest {
             //TABLE EMPLOYEES
             EmployeeOracleDao employeeDataOracle = new EmployeeOracleDao(oracleConn);
             EmployeeOracleDao employeeDataMSSQL = new EmployeeOracleDao(mssqlConn);
-            List<Employee> employeeDataList = employeeDataOracle.getAll();
-            employeeDataMSSQL.saveAll(employeeDataList);
+            exchangeData(employeeDataOracle, employeeDataMSSQL);
 
             //TABLE DEPARTMENTS
             DepartmentOracleDao departmentDataOracle = new DepartmentOracleDao(oracleConn);
             DepartmentOracleDao departmentDataMSSQL = new DepartmentOracleDao(mssqlConn);
-            List<Department> departmentDataList = departmentDataOracle.getAll();
-            departmentDataMSSQL.saveAll(departmentDataList);
+            exchangeData(departmentDataOracle, departmentDataMSSQL);
 
             //TABLE COUNTRIES
             CountryOracleDao countryDataOracle = new CountryOracleDao(oracleConn);
             CountryOracleDao countryDataMSSQL = new CountryOracleDao(mssqlConn);
-            List<Country> countryDataList = countryDataOracle.getAll();
-            countryDataMSSQL.saveAll(countryDataList);
+            exchangeData(countryDataOracle,countryDataMSSQL);
 
             //TABLE JOBS
             JobOracleDao jobDataOracle = new JobOracleDao(oracleConn);
             JobOracleDao jobDataMSSQL = new JobOracleDao(mssqlConn);
-            List<Job> jobDataList = jobDataOracle.getAll();
-            jobDataMSSQL.saveAll(jobDataList);
+            exchangeData(jobDataOracle,jobDataMSSQL);
 
             //TABLE JOB HISTORY
             JobHistoryOracleDao jhDataOracle = new JobHistoryOracleDao(oracleConn);
             JobHistoryOracleDao jhDataMSSQL = new JobHistoryOracleDao(mssqlConn);
-            List<JobHistory> jhDataList = jhDataOracle.getAll();
-            jhDataMSSQL.saveAll(jhDataList);
+            exchangeData(jhDataOracle,jhDataMSSQL);
 
             //TABLE LOCATIONS
             LocationOracleDao locationDataOracle = new LocationOracleDao(oracleConn);
             LocationOracleDao locationDataMSSQL = new LocationOracleDao(mssqlConn);
-            List<Location> locationDataList = locationDataOracle.getAll();
-            locationDataMSSQL.saveAll(locationDataList);
+            exchangeData(locationDataOracle,locationDataMSSQL);
 
             //TABLE REGIONS
             RegionOracleDao regionDataOracle = new RegionOracleDao(oracleConn);
             RegionOracleDao regionDataMSSQL = new RegionOracleDao(mssqlConn);
-            List<Region> regionDataList = regionDataOracle.getAll();
-            regionDataMSSQL.saveAll(regionDataList);
+            exchangeData(regionDataOracle,regionDataMSSQL);
 
-            String b = oracleMetadataHandler.getReferentialConstraints(Arrays.asList(referentialTables));
-            String constraints = b.replace("ENABLE", "");
-            System.out.println(constraints);
-            execute.query(constraints);
+            String referentialConstraints = oracleMetadataHandler.getReferentialConstraints(Arrays.asList(referentialTables));
+            referentialConstraints = referentialConstraints.replace("ENABLE", "");
+            System.out.println(referentialConstraints);
+            execute.query(referentialConstraints);
         } catch (SQLException e) {
             e.printStackTrace();
         }
+    }
+
+    private static void exchangeData(Dao source, Dao target) throws SQLException {
+        target.saveAll(source.getAll());
     }
 }
