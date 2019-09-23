e11780e4c72e469e7ff1fb4484d4bc46511e8a1f
Sebastian <tevas.dlr@gmail.com>
2019-09-22 21:33:58 -0600

    Clean act 7

--- tsql/tSQL7.sql
+++ tsql/tSQL7.sql
@@ -15,62 +15,62 @@ BEGIN
 END;
 
 
-
+GO
 CREATE FUNCTION getEmployeeName(
-    @id_emp INT 
+    @id_emp INT
 )
 RETURNS VARCHAR(200)
 AS
 BEGIN
     DECLARE @employee_name varchar(200);
- 
-    SELECT @employee_name = 
+
+    SELECT @employee_name =
 	first_name + ' ' + last_name
     FROM hr.employees
     WHERE employee_id = @id_emp;
-    
+
     RETURN @employee_name;
 END;
-
+GO
 
 CREATE FUNCTION getEmployeeYears(
-    @id_emp INT 
+    @id_emp INT
 )
 RETURNS FLOAT
 AS
 BEGIN
     DECLARE @emp_years SMALLINT;
- 
+
     SELECT @emp_years = YEAR(GETDATE()) - YEAR(hire_date)
     FROM hr.employees
     WHERE employee_id = @id_emp;
-    
+
     RETURN @emp_years;
 END;
-
+GO
 CREATE FUNCTION haveCommission(
-    @id_emp INT 
+    @id_emp INT
 )
 RETURNS FLOAT
 AS
 BEGIN
     DECLARE @commission FLOAT;
     DECLARE @flag SMALLINT;
- 
+
     SELECT @commission = commission_pct
     FROM hr.employees
     WHERE employee_id = @id_emp;
-    
+
     IF @commission = 0 OR @commission IS NULL BEGIN
         SET @flag = 0;
     END
     ELSE BEGIN
         SET @flag = 1;
-    END 
-    
+    END
+
     RETURN @flag;
 END;
-
+GO
 CREATE FUNCTION haveEmployees(
     @id_dept INT 
 )
